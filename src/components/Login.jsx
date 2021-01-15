2175696c122554d16fe16d0d39755c26cce2e4d6
shelcia <shelcia1999@gmail.com>
2021-01-15 20:39:47 +0530

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,24 +4,55 @@ import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <React.Fragment>
-      <h5>Login</h5>
-      <form>
-        <input
-          className="form-control"
-          type="text"
-          placeholder="enter your username"
-        />
-        <input
-          className="form-control"
-          type="password"
-          placeholder="enter your password"
-        />
-        <Link to={`/group/${Date.now()}`}>
-          <button type="button" className="btn btn-primary">
-            Enter
-          </button>
-        </Link>
-      </form>
+      <div className="bg-auth">
+        <div
+          className="container d-flex justify-content-center align-items-center"
+          style={{ height: "100vh" }}
+        >
+          <div className="row w-100">
+            <div className="col-sm-5">
+              <h1>n fn nvf nfmb nf nbf</h1>
+            </div>
+            <div className="col-sm-7">
+              <div className="card border border-0 rounded-0 card-auth">
+                <div className="card-body">
+                  <h1 className="mb-5">Login</h1>
+                  <form>
+                    <div className="form-check border">
+                      {/* <label className="form-check-label">Username</label> */}
+                      <input
+                        className="form-control"
+                        type="text"
+                        data-text="username"
+                        // placeholder="enter your username"
+                      />
+                    </div>
+                    <div className="form-check border">
+                      {/* <label className="form-check-label">Password</label> */}
+                      <input
+                        className="form-control"
+                        type="password"
+                        data-text="password"
+                        // placeholder="enter your password"
+                      />
+                    </div>
+                    <div className="text-center mt-5">
+                      <Link to={`/group/${Date.now()}`}>
+                        <button
+                          type="button"
+                          className="btn btn-outline-primary"
+                        >
+                          Enter
+                        </button>
+                      </Link>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
