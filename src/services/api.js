286cfd0f9c18b3074de89c1122b29055a15bfa6f
shelcia <shelcia1999@gmail.com>
2022-04-29 12:23:55 +0530

    🚀 deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,5 @@
 export const LOCALHOST_URL = "http://localhost:4050/api";
 
 export const SOCKET_URL = "http://localhost:4000/";
+
+export const VERCEL_URL = "https://kaagitham-backend.vercel.app/api";
